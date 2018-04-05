e0ff2f852c52dee5a4d833003f1333066f5b07ac
István Boros <boros.istvan@outlook.com>
2018-04-05 13:17:37 +0200

    Create GrandsPrixInteractor

---
+++ app/src/main/java/com/example/mobsoft/f1gpcalendar/interactor/GrandsPrix/GrandsPrixInteractor.java
@@ -0,0 +1,17 @@
+package com.example.mobsoft.f1gpcalendar.interactor.GrandsPrix;
+
+
+import com.example.mobsoft.f1gpcalendar.F1GPCalendarApplication;
+import com.example.mobsoft.f1gpcalendar.network.GrandsPrixApi;
+
+import javax.inject.Inject;
+
+public class GrandsPrixInteractor {
+
+    @Inject
+    GrandsPrixApi grandsPrixApi;
+
+    public GrandsPrixInteractor() { F1GPCalendarApplication.injector.inject(this); }
+
+    public void getGrandsPrix() {}
+}
