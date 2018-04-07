1da310049c9fa98f5ba2699a6352c00365d1d03a
István Boros <boros.istvan@outlook.com>
2018-04-07 11:02:47 +0200

    Add inject methods

--- app/src/main/java/com/example/mobsoft/f1gpcalendar/F1GPCalendarApplication.java
+++ app/src/main/java/com/example/mobsoft/f1gpcalendar/F1GPCalendarApplication.java
@@ -3,6 +3,8 @@ package com.example.mobsoft.f1gpcalendar;
 
 import android.app.Application;
 
+import com.example.mobsoft.f1gpcalendar.ui.UIModule;
+
 public class F1GPCalendarApplication extends Application {
 
     public static F1GPCalendarApplicationComponent injector;
@@ -11,6 +13,9 @@ public class F1GPCalendarApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        injector = DaggerF1GPCalendarApplicationComponent.builder().build();
+        injector = DaggerF1GPCalendarApplicationComponent.builder().
+                uIModule(
+                        new UIModule(this)
+                ).build();
     }
 }
--- app/src/main/java/com/example/mobsoft/f1gpcalendar/F1GPCalendarApplicationComponent.java
+++ app/src/main/java/com/example/mobsoft/f1gpcalendar/F1GPCalendarApplicationComponent.java
@@ -1,15 +1,20 @@
 package com.example.mobsoft.f1gpcalendar;
 
 import com.example.mobsoft.f1gpcalendar.interactor.GrandsPrix.GrandsPrixInteractor;
+import com.example.mobsoft.f1gpcalendar.interactor.Guesses.GuessesInteractor;
 import com.example.mobsoft.f1gpcalendar.interactor.InteractorModule;
 import com.example.mobsoft.f1gpcalendar.network.NetworkModule;
+import com.example.mobsoft.f1gpcalendar.ui.UIModule;
 
 import javax.inject.Singleton;
 
 import dagger.Component;
 
 @Singleton
-@Component(modules = {NetworkModule.class, InteractorModule.class})
+@Component(modules = {NetworkModule.class, InteractorModule.class,
+        UIModule.class})
 public interface F1GPCalendarApplicationComponent {
     void inject(GrandsPrixInteractor grandsPrixInteractor);
+
+    void inject(GuessesInteractor guessesInteractor);
 }
