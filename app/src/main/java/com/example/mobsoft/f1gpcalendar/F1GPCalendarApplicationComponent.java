0342e664d70419ee4f59d15230701ef1c8147efb
István Boros <boros.istvan@outlook.com>
2018-04-08 10:37:15 +0200

    Delete network module dependencie after rebase

--- app/src/main/java/com/example/mobsoft/f1gpcalendar/F1GPCalendarApplicationComponent.java
+++ app/src/main/java/com/example/mobsoft/f1gpcalendar/F1GPCalendarApplicationComponent.java
@@ -3,7 +3,6 @@ package com.example.mobsoft.f1gpcalendar;
 import com.example.mobsoft.f1gpcalendar.interactor.GrandsPrix.GrandsPrixInteractor;
 import com.example.mobsoft.f1gpcalendar.interactor.Guesses.GuessesInteractor;
 import com.example.mobsoft.f1gpcalendar.interactor.InteractorModule;
-import com.example.mobsoft.f1gpcalendar.network.NetworkModule;
 import com.example.mobsoft.f1gpcalendar.ui.UIModule;
 
 import javax.inject.Singleton;
@@ -11,8 +10,7 @@ import javax.inject.Singleton;
 import dagger.Component;
 
 @Singleton
-@Component(modules = {NetworkModule.class, InteractorModule.class,
-        UIModule.class})
+@Component(modules = {InteractorModule.class, UIModule.class})
 public interface F1GPCalendarApplicationComponent {
     void inject(GrandsPrixInteractor grandsPrixInteractor);
 
