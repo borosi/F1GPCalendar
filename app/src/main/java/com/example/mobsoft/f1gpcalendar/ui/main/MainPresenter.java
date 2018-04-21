7c237499f70105682c78cdd1214c6884c02a5e65
István Boros <boros.istvan@outlook.com>
2018-04-21 14:59:44 +0200

    Inject network executor to MainPresenter

--- app/src/main/java/com/example/mobsoft/f1gpcalendar/ui/main/MainPresenter.java
+++ app/src/main/java/com/example/mobsoft/f1gpcalendar/ui/main/MainPresenter.java
@@ -1,12 +1,19 @@
 package com.example.mobsoft.f1gpcalendar.ui.main;
 
+import com.example.mobsoft.f1gpcalendar.di.Network;
 import com.example.mobsoft.f1gpcalendar.interactor.GrandsPrix.GrandsPrixInteractor;
 import com.example.mobsoft.f1gpcalendar.ui.Presenter;
+
+import java.util.concurrent.Executor;
 
 import javax.inject.Inject;
 
 
 public class MainPresenter extends Presenter<MainScreen> {
+
+    @Inject
+    @Network
+    Executor networkExecutor;
 
     @Inject
     GrandsPrixInteractor grandsPrixInteractor;
