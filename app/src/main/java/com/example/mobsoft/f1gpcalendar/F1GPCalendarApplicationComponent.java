68424ac0c94b47a2634fcd58a003bc7e1373c134
István Boros <boros.istvan@outlook.com>
2018-04-21 14:58:13 +0200

    Add NetworkModule.class to ApplicationComponent

--- app/src/main/java/com/example/mobsoft/f1gpcalendar/F1GPCalendarApplicationComponent.java
+++ app/src/main/java/com/example/mobsoft/f1gpcalendar/F1GPCalendarApplicationComponent.java
@@ -3,6 +3,7 @@ package com.example.mobsoft.f1gpcalendar;
 import com.example.mobsoft.f1gpcalendar.interactor.GrandsPrix.GrandsPrixInteractor;
 import com.example.mobsoft.f1gpcalendar.interactor.Guesses.GuessesInteractor;
 import com.example.mobsoft.f1gpcalendar.interactor.InteractorModule;
+import com.example.mobsoft.f1gpcalendar.network.NetworkModule;
 import com.example.mobsoft.f1gpcalendar.ui.UIModule;
 
 import javax.inject.Singleton;
@@ -10,7 +11,7 @@ import javax.inject.Singleton;
 import dagger.Component;
 
 @Singleton
-@Component(modules = {InteractorModule.class, UIModule.class})
+@Component(modules = {InteractorModule.class, UIModule.class, NetworkModule.class})
 public interface F1GPCalendarApplicationComponent {
     void inject(GrandsPrixInteractor grandsPrixInteractor);
 
